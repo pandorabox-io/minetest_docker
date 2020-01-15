7bc4fe10af98e12561024dda6f53b33db754c85b
Thomas Rudin <thomas@rudin.io>
2020-01-15 12:01:32 +0100

    crashlogs folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p crashlogs
+mkdir -p /crashlogs
 ulimit -c unlimited
 /usr/local/bin/minetestserver --config /etc/minetest/minetest.conf
 
 DATE_FMT=`date +"%Y-%m-%d_%H-%M"`
-tail -n1000 debug.txt | grep ERROR > crashlogs/crash_${DATE_FMT}.txt
+tail -n1000 /root/.minetest/debug.txt | grep ERROR > /crashlogs/crash_${DATE_FMT}.txt
