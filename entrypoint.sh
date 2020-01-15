388e518a35b52897cfff94d21cabe87c58278fee
Thomas Rudin <thomas@rudin.io>
2020-01-15 15:07:03 +0100

    ignore return value

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /crashlogs
 ulimit -c unlimited
-/usr/local/bin/minetestserver --config /etc/minetest/minetest.conf
+/usr/local/bin/minetestserver --config /etc/minetest/minetest.conf || true
 
 DATE_FMT=`date +"%Y-%m-%d_%H-%M"`
 tail -n1000 /root/.minetest/debug.txt | grep ERROR > /crashlogs/crash_${DATE_FMT}.txt
