c34fd3e216dbcdfcb53876ba2f73f1ff4bde811c
Thomas Rudin <thomas@rudin.io>
2020-01-15 07:42:48 +0100

    create crash logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mkdir -p crashlogs
 ulimit -c unlimited
 /usr/local/bin/minetestserver --config /etc/minetest/minetest.conf
 
+DATE_FMT=`date +"%Y-%m-%d_%H-%M"`
+tail -n1000 debug.txt | grep ERROR > crashlogs/crash_${DATE_FMT}.txt
