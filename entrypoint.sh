758dc7fd432666d7b85741ef6b8f0619e7ac0f89
Thomas Rudin <thomas@rudin.io>
2020-01-20 21:18:11 +0100

    exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,8 @@
 mkdir -p /crashlogs
 ulimit -c unlimited
 
-minetestserver --config /data/minetest.conf --world /data/world/ --quiet &
-child_pid=$!
+exec minetestserver --config /data/minetest.conf --world /data/world/ --quiet &
 
-exit_script() {
-        kill $child_pid
-}
-
-trap exit_script SIGINT SIGTERM
-
-sleep inf
 
 DATE_FMT=`date +"%Y-%m-%d_%H-%M"`
 tail -n1000 /root/.minetest/debug.txt | grep ERROR > /crashlogs/crash_${DATE_FMT}.txt
