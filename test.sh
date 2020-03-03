653d4b08f5443514329b0d089fc38f6e084f39af
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-03 08:34:05 +0100

    add integrated test

--- test.sh
+++ test.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+# simple integration test
 
-docker run --rm -it \
-	-v "$(pwd)/data:/data" \
-	buckaroobanzay/minetest
+CFG=/tmp/minetest.conf
+MTDIR=/tmp/mt
+WORLDDIR=${MTDIR}/worlds/world
+
+cat <<EOF > ${CFG}
+# test config
+EOF
+
+
+
+mkdir -p ${WORLDDIR}
+chmod 777 ${MTDIR} -R
+docker run --rm -i \
+	-v ${CFG}:/etc/minetest/minetest.conf:ro \
+	-v ${MTDIR}:/var/lib/minetest/.minetest \
+	-v $(pwd)/test_mod:/var/lib/minetest/.minetest/worlds/world/worldmods/test_mod \
+	buckaroobanzay/minetest:${BRANCH}
+
+test -f ${WORLDDIR}/integration_test.json && exit 0 || exit 1
