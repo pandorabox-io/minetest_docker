e83f27f6ef5694a8fc149af8f7711be803abe406
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-28 17:58:49 +0100

    disable async map saving (for testing)

--- test.sh
+++ test.sh
@@ -15,6 +15,7 @@ docker run --rm -i \
 	-v ${CFG}:/data/minetest.conf:ro \
 	-v ${MT_WORLD_DIR}:/data/world \
 	-v $(pwd)/test_mod:/data/world/worldmods/test_mod \
+	-e ADDITIONAL_PARAMS= \
 	buckaroobanzay/minetest:${BRANCH}
 
 test -f ${MT_WORLD_DIR}/integration_test.json && exit 0 || exit 1
