abfef0ae0f8d3c247f16b7658c5fddc61a918271
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-02-28 11:49:37 +0100

    async stuff

--- patch-engine.sh
+++ patch-engine.sh
@@ -13,14 +13,8 @@ cat patches/lua_profiler.patch | patch -p1
 echo "Disable some timestamp shenanigans"
 cat patches/disable_timestamps.patch | patch -p1
 
-echo "Deferrend mapsending patch"
-cat patches/minetest_deferred_send.patch | patch -p1
-
 echo "util/Threadpool.h"
 cat patches/util_threadpool.patch | patch -p1
 
-echo "Metrics and async mapsending"
-cat patches/minetest_metrics_and_mapsending.patch | patch -p1
-
-echo "async Map::saveBlock()"
-cat patches/minetest_async_mapsave.patch | patch -p1
+echo "async map saving,sending and metrics"
+cat patches/minetest_async.patch | patch -p1
