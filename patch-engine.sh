483f20b9635d7e1ad262c8a0e423f8cd65312567
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-10-17 07:32:08 +0200

    add patch to remove some frequent pg verify and ping calls

--- patch-engine.sh
+++ patch-engine.sh
@@ -17,3 +17,6 @@ cat patches/minetest_deferred_send.patch | patch -p1
 
 echo "Patch: get_player_info() with debug info"
 cat patches/player_debug_info.patch | patch -p1
+
+echo "Patch: disable frequent pg verify and ping"
+cat patches/disable_pg_verify_ping.patch | patch -p1
